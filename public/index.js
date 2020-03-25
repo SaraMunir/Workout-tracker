4ddba08b79752617e8361a00cda19983514c4a16
Sara Munir <saramunir011@gmail.com>
2020-03-25 10:15:50 -0400

    fixed continue button

--- public/index.js
+++ public/index.js
@@ -3,15 +3,15 @@ init();
 async function init() {
   if (location.search.split("=")[1] === undefined) {
     // const workout = await API.getLastWorkout();
-    const workout = await $.get( '/api/lastworkouts');
-    const workout2 = await $.get( '/api/uncompletedworkouts');
-    console.log('my last workout is ',workout)
+    // const workout = await $.get( '/api/lastworkouts');
+    // const workout2 = await $.get( '/api/uncompletedworkouts');
+    // console.log('my uncompleted workout is ', workout2)
 
-    if (workout2) {
-      location.search = "?id=" + workout2._id;
-    } else {
-      document.querySelector("#continue-btn").classList.add("d-none")
-    }
+    // if (workout2 = null ) {
+    //   // $('#cntBtn')
+    //   document.querySelector("#cntBtn-btn").classList.add("d-none")
+    // }
+    
   }
 }
 
